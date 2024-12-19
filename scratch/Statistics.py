37c180ce6ff56680a89ef2c23b6e00a3ec57eaee
Diyasingh03 <63916265+Diyasingh03@users.noreply.github.com>
2024-12-19 10:16:10 -0500

    Module creation

---
+++ scratch/Statistics.py
@@ -0,0 +1,73 @@
+from collections import Counter
+import matplotlib.pyplot as plt
+from typing import List
+from scratch.Lin_Alg import sum_of_squares
+num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+friend_counts = Counter(num_friends)
+xs = range(101) # largest value is 100
+ys = [friend_counts[x] for x in xs] # height is just # of friends
+plt.bar(xs, ys)
+plt.axis([0, 101, 0, 25])
+plt.title("Histogram of Friend Counts")
+plt.xlabel("# of friends")
+plt.ylabel("# of people")
+plt.show()
+
+num_points = len(num_friends) # 204
+largest_value = max(num_friends) # 100
+smallest_value = min(num_friends) # 1
+
+sorted_values = sorted(num_friends)
+smallest_value = sorted_values[0] # 1
+second_smallest_value = sorted_values[1] # 1
+second_largest_value = sorted_values[-2] # 49
+
+def mean(xs: List[float]) -> float:
+    return sum(xs) / len(xs)
+mean(num_friends) # 7.333333
+
+#_ is a private function
+def _median_odd(xs: List[float]) -> float:
+    return sorted(xs)[len(xs) // 2]
+
+def _median_even(xs: List[float]) -> float:
+    sorted_xs = sorted(xs)
+    hi_midpoint = len(xs) // 2
+    return (sorted_xs[hi_midpoint - 1] + sorted_xs[hi_midpoint]) / 2
+
+def median(v: List[float]) -> float:
+    return _median_even(v) if len(v) % 2 == 0 else _median_odd(v)
+assert median([1, 10, 2, 9, 5]) == 5
+assert median([1, 9, 2, 10]) == (2 + 9) / 2
+print(median(num_friends)) # 6
+
+def quantile(xs: List[float], p: float) -> float:
+    p_index = int(p * len(xs))
+    return sorted(xs)[p_index]
+assert quantile(num_friends, 0.10) == 1
+assert quantile(num_friends, 0.25) == 3
+assert quantile(num_friends, 0.75) == 9
+assert quantile(num_friends, 0.90) == 13
+
+def mode(x: List[float]) -> List[float]:
+    counts = Counter(x)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.items() if count == max_count]
+assert set(mode(num_friends)) == {1, 6}
+
+#range exists in python so we name this data_range
+def data_range(xs: List[float]) -> float:
+    return max(xs) - min(xs)
+assert data_range(num_friends) == 99
+
+def de_mean(xs: List[float]) -> List[float]:
+    x_bar = mean(xs)
+    return [x - x_bar for x in xs]
+
+def variance(xs: List[float]) -> float:
+    assert len(xs) >= 2, "variance requires at least two elements"
+    n = len(xs)
+    deviations = de_mean(xs)
+    return sum_of_squares(deviations) / (n - 1)
+assert 81.54 < variance(num_friends) < 81.55
